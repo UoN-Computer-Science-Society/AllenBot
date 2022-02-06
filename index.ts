069da6a2a0baaed2db862c669942e603189c4cf5
xenonial98 <mirza898@gmail.com>
2022-02-06 16:26:18 +0800

    Added mongodb connection

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import {Client, Collection, Intents} from 'discord.js'
 import dotenv from 'dotenv'
+import {MongoClient} from 'mongodb'
 import fs from 'fs'
 import * as commandModules from "./commands"
 
@@ -21,13 +22,32 @@ const commands = Object(commandModules)
 client.once('ready', async () => {
   console.log(`Allen's ready!`)  
 });
-
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) {return;}
   const {commandName} = interaction;
   commands[commandName].execute(interaction, client) // passes client as well to commands 
 })
 
+// Setup mongodb connection
+if (!process.env.MONGO_URI){
+  console.error('Mongo URI not found in Environmetn variables.');
+  process.exit(1);
+}
+const mongoClient = new MongoClient(process.env.MONGO_URI!)
+async function run() {
+  try {
+    await mongoClient.connect();
+    mongoClient.db().admin().listDatabases()
+      .then(dbs => {
+        console.log("Mongo Databases:", dbs)
+      });
+  } catch (err) {
+    console.error(err);
+  }
+}
+run().catch(console.dir)
+
+// Bot Login
 if (process.env.TOKEN)
   client.login(process.env.TOKEN);
 else {
@@ -37,6 +57,8 @@ else {
 
 // Graceful closing of Allen
 process.on('SIGINT', function () {
+  console.log('Disconnecting from MongoDB...')
+  mongoClient.close();
   process.stdout.write("Disconnecting from discord...\n");
   client.destroy();
   process.stdout.write("Allen is now going to sleep o/");
